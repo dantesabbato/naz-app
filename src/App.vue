ab707b4f7db9014cc68998b91565220bff37cf25
MaxKagami <max.kagami@gmail.com>
2020-06-28 23:21:44 +0300

    Added Vuex

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div id='app'>
-    <router-view/>
+    <router-view :contacts='contacts'/>
+    <Footer/>
   </div>
 </template>
 
 <script>
+  import Footer from './components/footer'
   export default {
+    components: { Footer },
+    data: () => ({
+    })
   }
 </script>
 
@@ -13,19 +18,24 @@
   @font-face
     font-family: "PipoRegular"
     src: url('assets/fonts/pipo_regular.ttf') format('truetype')
-
   @font-face
     font-family: "PipoLight"
     src: url('assets/fonts/pipo_light.ttf') format('truetype')
-
   @font-face
     font-family: "Cedra4F"
     src: url('assets/fonts/cedra4f_wide.ttf') format('truetype')
-
   @font-face
     font-family: "Cedra4FLight"
     src: url('assets/fonts/cedra4f_wide_ultralight.ttf') format('truetype')
 
+  html, body, #app
+    height: 100%
   #app
     font-family: 'Cedra4F'
+    display: grid !important
+    grid-template-rows: 1fr auto
+  #footer
+    grid-row-start: 2
+    grid-row-end: 3
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,22 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import BootstrapVue from 'bootstrap-vue'
-import Vicon from 'vue-awesome/components/Icon'
-import Main from './layouts/main'
-import Admin from './layouts/admin'
-
+//\\//\\//\\ Libraries //\\//\\//\\
 import 'vue-awesome/icons'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import BootstrapVue from 'bootstrap-vue'
+import Vicon from 'vue-awesome/components/Icon'
+import Vue from 'vue'
+//\\//\\//\\ Modules //\\//\\//\\
+import router from './router'
+import { index } from './stores'
+//\\//\\//\\ Components //\\//\\//\\
+import App from './App.vue'
+import Admin from './layouts/admin'
+import Main from './layouts/main'
 
 Vue.config.productionTip = false
-Vue.component('main-layout', Main)
 Vue.component('admin-layout', Admin)
+Vue.component('main-layout', Main)
 Vue.component('v-icon', Vicon)
 Vue.use(BootstrapVue)
 
 new Vue({
   router,
+  store: index,
   render: h => h(App),
 }).$mount('#app')
