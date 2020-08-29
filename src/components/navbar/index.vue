b8c9b1162fb13925a4ab1aa4e315a6dd2fc7b9f0
MaxKagami <max.kagami@gmail.com>
2020-08-29 23:58:43 +0300

    :art: Moved components

--- src/components/navbar/index.vue
+++ src/components/navbar/index.vue
@@ -1,21 +1,4 @@
 <template>
-  <b-navbar toggleable="lg">
-    <b-navbar-toggle target="nav-collapse">
-      <template v-slot:default="{ expanded }">
-        <v-icon v-if="expanded" name="times" scale="2" class="white"/>
-        <v-icon v-else name="bars" scale="2" class="black"/>
-      </template>
-    </b-navbar-toggle>
-    <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav align="center">
-        <b-nav-item to="/women">Women</b-nav-item>
-        <b-nav-item to="/men">Men</b-nav-item>
-        <b-nav-item to="/become_naz">Become NAZ</b-nav-item>
-        <b-nav-item to="/info">Info</b-nav-item>
-        <b-nav-item to="/contacts">Contacts</b-nav-item>
-      </b-navbar-nav>
-    </b-collapse>
-  </b-navbar>
 </template>
 
 <script>export default { name: "navbar" }</script>
